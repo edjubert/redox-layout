4f7bbc6b00177d5388ae8355515b56ed1656af0f
edjubert <edouard@kontestapp.com>
2021-01-06 12:05:19 +0100

    fix: symbols access

--- complex_calls.c
+++ complex_calls.c
@@ -5,16 +5,19 @@ bool complex_calls(uint16_t keycode, keyrecord_t *record) {
           SEND_STRING(SS_DOWN(X_LGUI)SS_DOWN(X_LALT)SS_TAP(X_BSLASH)SS_UP(X_LGUI)SS_UP(X_LALT));
         }
         return false;
+        break;
       case ALFRED:
         if (record->event.pressed) {
           SEND_STRING(SS_DOWN(X_LALT)SS_TAP(X_SPACE)SS_UP(X_LALT));
         }
         return false;
+        break;
       case PASTE:
         if (record->event.pressed) {
           SEND_STRING(SS_DOWN(X_LGUI)SS_LSFT("V")SS_UP(X_LGUI));
         }
         return false;
+        break;
   }
 }
 
