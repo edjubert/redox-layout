23eb7b0d8056f61d6725257cb2b5386b81441bb2
edjubert <edouard@kontestapp.com>
2021-01-06 11:22:49 +0100

    fix: numbers for master

--- edjubert.h
+++ edjubert.h
@@ -1,10 +1,80 @@
 #ifndef __EDJUBERT_H__
   #define __EDJUBERT_H__
 
+  #ifdef AUDIO_ENABLE
+    float tone_colemak[][2] = SONG(ZELDA_TREASURE);
+  #endif
+
+  extern keymap_config_t keymap_config;
+
+  // Each layer gets a name for readability, which is then used in the keymap matrix below.
+  // The underscores don't mean anything - you can have a layer called STUFF or any other name.
+  // Layer names don't all need to be of the same length, obviously, and you can also skip them
+  // entirely and just use numbers.
+  #define _QWERTY 0
+  #define _SYMB 1
+  #define _NAV 2
+  #define _INTER 3
+  #define _ADJUST 4
+
+  enum custom_keycodes {
+      COLEMAK = SAFE_RANGE,
+      SYMB,
+      NAV,
+      INTER,
+      ADJUST,
+      // These use process_record_user()
+      M_BRACKET_LEFT,
+      M_BRACKET_RIGHT,
+      M_SBRACKET_LEFT,
+      M_SBRACKET_RIGHT,
+      M_CBRACKET_LEFT,
+      M_CBRACKET_RIGHT,
+      M_SUPERIOR,
+      M_INFERIOR,
+      M_0_I,
+      M_1_I,
+      M_2_I,
+      M_3_I,
+      M_4_I,
+      M_5_I,
+      M_6_I,
+      M_7_I,
+      M_8_I,
+      M_9_I,
+    INV_1P,
+    ALFRED,
+    PASTE
+  };
+
+
+  // Shortcut to make keymap more readable
+  // To declare a keycode for long press:
+  // #define KC_MYKEYCODE LT(_LAYERNAME, KC_STDKEY)
+  // #define KC_SYQT  LT(_SYMB,KC_QUOT)
+
+  #define SYM_L    MO(_SYMB)
+  #define MO_INTR  MO(_INTER)
+  
+  #define TT_ADJ   TT(_ADJUST)
+  #define TT_INTER TT(_INTER)
+  #define TT_NAV   TT(_NAV)
+  
+  #define KC_BRL  M_BRACKET_LEFT      // (
+  #define KC_BRR  M_BRACKET_RIGHT     // )
+  #define KC_SBRL M_SBRACKET_LEFT     // [
+  #define KC_SBRR M_SBRACKET_RIGHT    // ]
+  #define KC_CBRL M_CBRACKET_LEFT     // {
+  #define KC_CBRR M_CBRACKET_RIGHT    // }
+  #define KC_INF  M_INFERIOR          // <
+  #define KC_SUP  M_SUPERIOR          // >
+
   bool complex_calls(uint16_t keycode, keyrecord_t *record);
   bool invert_keys(uint16_t keycode, keyrecord_t *record);
   bool force_brackets(uint16_t keycode, keyrecord_t *record);
   bool held_shift(uint16_t keycode, keyrecord_t *record);
   bool process_record_user(uint16_t keycode, keyrecord_t *record);
+  uint32_t layer_state_set_user(uint32_t state);
+  void persistent_default_layer_set(uint16_t default_layer);
 
 #endif
--- keymap.c
+++ keymap.c
@@ -1,75 +1,12 @@
 #include QMK_KEYBOARD_H
 #include "edjubert.h"
 
-extern keymap_config_t keymap_config;
-
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-#define _QWERTY 0
-#define _SYMB 1
-#define _NAV 2
-#define _INTER 3
-#define _ADJUST 4
-
-enum custom_keycodes {
-	COLEMAK = SAFE_RANGE,
-	SYMB,
-	NAV,
-	INTER,
-	ADJUST,
-	// These use process_record_user()
-	M_BRACKET_LEFT,
-	M_BRACKET_RIGHT,
-	M_SBRACKET_LEFT,
-	M_SBRACKET_RIGHT,
-	M_CBRACKET_LEFT,
-	M_CBRACKET_RIGHT,
-    M_SUPERIOR,
-    M_INFERIOR,
-    M_0_I,
-    M_1_I,
-    M_2_I,
-    M_3_I,
-    M_4_I,
-    M_5_I,
-    M_6_I,
-    M_7_I,
-    M_8_I,
-    M_9_I,
-  INV_1P,
-  ALFRED,
-  PASTE
-};
-
-
-// Shortcut to make keymap more readable
-// To declare a keycode for long press:
-// #define KC_MYKEYCODE LT(_LAYERNAME, KC_STDKEY)
-// #define KC_SYQT  LT(_SYMB,KC_QUOT)
-
-#define SYM_L    MO(_SYMB)
-#define MO_INTR  MO(_INTER)
-
-#define TT_ADJ   TT(_ADJUST)
-#define TT_INTER TT(_INTER)
-#define TT_NAV   TT(_NAV)
-
-#define KC_BRL  M_BRACKET_LEFT      // (
-#define KC_BRR  M_BRACKET_RIGHT     // )
-#define KC_SBRL M_SBRACKET_LEFT     // [
-#define KC_SBRR M_SBRACKET_RIGHT    // ]
-#define KC_CBRL M_CBRACKET_LEFT     // {
-#define KC_CBRR M_CBRACKET_RIGHT    // }
-#define KC_INF  M_INFERIOR          // <
-#define KC_SUP  M_SUPERIOR          // >
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_GRV  ,KC_7    ,KC_5    ,KC_3    ,KC_1    ,KC_9    ,                                            KC_0    ,KC_2    ,KC_4    ,KC_6    ,KC_8    ,KC_MINS ,
+     KC_GRV  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                                            KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_MINS ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,PASTE   ,                           INV_1P ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_BSLS ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
@@ -137,41 +74,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______ ,XXXXXXX ,XXXXXXX ,XXXXXXX ,     XXXXXXX ,    XXXXXXX ,_______ ,        _______ ,XXXXXXX ,    XXXXXXX ,     _______ ,XXXXXXX ,XXXXXXX ,_______
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   )
-
 };
 
-#ifdef AUDIO_ENABLE
-float tone_colemak[][2]     = SONG(ZELDA_TREASURE);
-#endif
-
-void persistent_default_layer_set(uint16_t default_layer) {
-	eeconfig_update_default_layer(default_layer);
-	default_layer_set(default_layer);
-}
-
-// Set unicode in Mac OS.
-void eeconfig_init_user(void) {
-  set_unicode_input_mode(UC_OSX);
-};
-
-// Change LED colors depending on the layer.
-uint32_t layer_state_set_user(uint32_t state) {
-	switch (biton32(state)) {
-		case _SYMB:
-			rgblight_setrgb_orange();
-			break;
-		case _NAV:
-			rgblight_setrgb_springgreen();
-			break;
-		case _INTER:
-			rgblight_setrgb_teal();
-			break;
-		case _ADJUST:
-			rgblight_setrgb_red();
-			break;
-		default: //  for any other layers, or the default layer
-			rgblight_setrgb_yellow();
-			break;
-	}
-	return state;
-};
